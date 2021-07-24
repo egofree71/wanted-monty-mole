1cd98e14b8eac78bc1035142cffd2104740ae41f
egofree71 <philippedepol@gmail.com>
2021-07-24 15:29:17 +0200

    Increase score when player takes the bucket.

--- Assets/Scripts/Entities/Bucket.cs
+++ Assets/Scripts/Entities/Bucket.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class Bucket : MonoBehaviour
 {
+  // The gameobject GameManager manages the game
+  private GameManager gameManager;
+
+  void Start()
+  {
+    gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+  }
+
   // If the player collides with the object
   private void OnTriggerEnter2D(Collider2D collision)
   {
     Destroy(gameObject);
-  }
-
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
+    gameManager.IncreaseScore();
   }
 }
