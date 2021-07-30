de57602e1449b5ad94cdb371b9f2dd2f97701486
egofree71 <philippedepol@gmail.com>
2021-07-30 16:24:05 +0200

    Move gas.

---
+++ Assets/Scripts/Entities/Gas.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gas : MonoBehaviour
+{
+  // Is gas entity moving to the right ?
+  bool isMovingRight = true;
+  // The maximum distance
+  int maxDistance = 50;
+  float moveStep = 2.0f;
+  // The current distance
+  int currentDistance = 0;
+
+  void Update()
+  {
+    if (isMovingRight)
+    {
+      // If the gas has not reached the maximum distance, move to the right
+      if (currentDistance < maxDistance * moveStep)
+      {
+        transform.position = new Vector2(transform.position.x + moveStep, transform.position.y);
+        currentDistance++;
+      }
+      else
+      {
+        isMovingRight = !isMovingRight;
+      }
+    }
+    else
+    {
+      // If the gas has not reached the minimum distance, move to the left
+      if (currentDistance > 0)
+      {
+        transform.position = new Vector2(transform.position.x - moveStep, transform.position.y);
+        currentDistance--;
+      }
+      else
+      {
+        isMovingRight = !isMovingRight;
+      }
+    }
+  }
+}
