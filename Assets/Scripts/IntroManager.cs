8bd4e45a348bc780a626608de7b0dcc7c05aee5a
egofree71 <philippedepol@gmail.com>
2022-02-11 20:36:43 +0100

    Start moving the logo.

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -9,10 +9,25 @@ public class IntroManager : MonoBehaviour
   // The object used to display the logo
   public GameObject logo;
 
-  // Start is called before the first frame update
   void Start()
   {
+    StartCoroutine(MoveLogo());
+  }
 
+  // Move logo and mole
+  IEnumerator MoveLogo()
+  {
+    // Get logo width
+    Vector3 sizeLogo = logo.GetComponent<SpriteRenderer>().bounds.size;
+    int logoWidth = (int)sizeLogo.x;
+
+    // Move logo to the center
+    while (logo.transform.position.x + logoWidth/2> 0)
+    {
+      logo.transform.Translate(Vector3.left);
+      yield return null;
+    }
+    
   }
 
   // Update is called once per frame
@@ -20,7 +35,6 @@ public class IntroManager : MonoBehaviour
   {
     // Start playing if the player press the space key
     if (Input.GetKeyDown(KeyCode.Space))
-      SceneManager.LoadScene("Main");
-    
+      SceneManager.LoadScene("Main");  
   }
 }
