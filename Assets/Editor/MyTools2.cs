2bb9c574072c82624aad930da2b290d6a0810dfb
egofree71 <philippedepol@gmail.com>
2022-02-16 21:47:29 +0100

    Add second row.

--- Assets/Editor/MyTools2.cs
+++ Assets/Editor/MyTools2.cs
@@ -15,24 +15,40 @@ public partial class MyTools
 
     // Add a container which contains all squares objects
     GameObject.DestroyImmediate(GameObject.Find("Squares"));
-    GameObject squares = new GameObject("Squares");
+    GameObject container = new GameObject("Squares");
 
+    // Load the square prefab
+    UnityEngine.Object square = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Square.prefab", typeof(GameObject));
+
+    // Add rows made of squares
     for (int column = 0; column < 40; column++)
     {
       int positionX;
 
+      // Add first row
       positionX = (column * pixelsPerTile) - 650;
-      // Create a new square object
-      UnityEngine.Object square = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Square.prefab", typeof(GameObject));
-      GameObject newSquare = GameObject.Instantiate(square, new Vector2(positionX, 300f), Quaternion.identity) as GameObject;
-      // Set the color
-      newSquare.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
-      Square squareScript = newSquare.GetComponent<Square>();
-      squareScript.column = column;
+      Vector2 position = new Vector2(positionX, 300f);
+      instantiateSquare(square, 0, column, position, container);
 
-      // Add the prefab into the squares object
-      newSquare.transform.parent = squares.transform;
+      // Add second row
+      position = new Vector2(positionX, 300f - 23 * pixelsPerTile);
+      instantiateSquare(square, 25, column, position, container);
     }
   }
 
+  // Instantiate a square object for a given row and column
+  private static void instantiateSquare(UnityEngine.Object square, int row, int column, Vector2 position, GameObject container)
+  {
+    // Create a new square object
+    GameObject newSquare = GameObject.Instantiate(square, position, Quaternion.identity) as GameObject;
+    // Set the color
+    newSquare.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
+    // Set row and column
+    Square squareScript = newSquare.GetComponent<Square>();
+    squareScript.row = row;
+    squareScript.column = column;
+
+    // Add the prefab into the squares container
+    newSquare.transform.parent = container.transform;
+  }
 }
